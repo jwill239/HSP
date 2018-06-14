29e638a6476978fa139d7d68dd37996464ccd7b2
Salandin, Emanuele <emanuele.salandin@phox-eng.it>
2018-06-14 16:45:30 +0200

    uso variabili floating point

--- eclipse/hsp.pl
+++ eclipse/hsp.pl
@@ -28,27 +28,27 @@ event_t(List, Step, T) :-
 % Il formato e': [carico, pos1, pos2, ..., posN, scarico]
 
 lin0(Removal, Entry) :-
-	Removal=[R1|_], R1 #= 0,
-	Entry=[E1|_], E1 #= 0,
-	last(A, Removal), last(B, Entry), A #= B.
+	Removal=[R1|_], R1 $= 0,
+	Entry=[E1|_], E1 $= 0,
+	last(A, Removal), last(B, Entry), A $= B.
 
 % vincolo: tra la estrazione da una posizione e l'inserimento nella successiva posizione deve esserci il tempo di sollevamento, traslazione, abbassamento.
 lin1(Removal, Entry) :- findall(S, step(S), StepsList), step_unload(Last), append(StepsList, [Last], StepsListExt), maplist(lin1(Removal, Entry), StepsListExt).
 
 lin1(Removal, Entry,S) :-
 	hoist_lift_t(TLift), hoist_lift_t(TLower),
-	succ(PredS, S), event_t(Removal, PredS, TA), hoist_move_t(PredS, S, TMove), event_t(Entry, S, TB), TB #= TA+ TLift+ TMove + TLower.
+	succ(PredS, S), event_t(Removal, PredS, TA), hoist_move_t(PredS, S, TMove), event_t(Entry, S, TB), TB $= TA+ TLift+ TMove + TLower.
 	
 % vincolo: tempo di permanenza in una posizione
 lin2(Removal, Entry) :- findall(S, step(S), StepsList), maplist(lin2(Removal, Entry), StepsList).
 
 lin2(Removal, Entry, S) :-
 	event_t(Entry, S, TIn), event_t(Removal, S, TOut), step_t(S, Tmin, Tmax),
-	TIn+Tmin #=< TOut,
-	TIn+Tmax #>= TOut.
+	TIn+Tmin $=< TOut,
+	TIn+Tmax $>= TOut.
 
 % vincolo: il tempo di permanenza di una barra nel sistema non puo' superare NumJobs cicli (perche' ad ogni ciclo viene rimossa una barra)
-lin3(Removal, NumJobs, Period) :- step_unload(ULStep), event_t(Removal, ULStep, LastT), hoist_move_t(ULStep,0,TRet), LastT+TRet #=< NumJobs * Period.
+lin3(Removal, NumJobs, Period) :- step_unload(ULStep), event_t(Removal, ULStep, LastT), hoist_move_t(ULStep,0,TRet), LastT+TRet $=< NumJobs * Period.
 
 % il carro deve avere il tempo di andare da una posizione all'altra.
 % in questi calcoli il carro e' vuoto (non trasporta barre), quindi ignoriamo i tempi di sollevamento/abbassamento.
@@ -59,7 +59,7 @@ disj1(Removal, Entry, Period, Step1, Step2, K) :-
 	event_t(Entry, SuccS1, TEntrySuccS1),
 	hoist_move_t(SuccS1, Step2, TMove),
 	event_t(Removal, Step2, TRemovalS2),
-	TEntrySuccS1 + TMove + K*Period #=< TRemovalS2.
+	TEntrySuccS1 + TMove + K*Period $=< TRemovalS2.
 	
 % caso 2: la barra viene estratta dallo step1 dopo che e' stata estratta dallo step2
 disj1(Removal, Entry, Period, Step1, Step2, K) :-
@@ -67,7 +67,7 @@ disj1(Removal, Entry, Period, Step1, Step2, K) :-
 	event_t(Entry, SuccS2, TEntrySuccS2),
 	hoist_move_t(SuccS2, Step1, TMove),
 	event_t(Removal, Step1, TRemovalS1),
-	TEntrySuccS2 + TMove #=< TRemovalS1 + K*Period.
+	TEntrySuccS2 + TMove $=< TRemovalS1 + K*Period.
 
 % elenco di tutte le coppie di vasche (step).
 list_disj(L) :-
