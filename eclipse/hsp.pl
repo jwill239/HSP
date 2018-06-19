2e7ce9806de5560d848f9cff8f9bf9722b7bf03d
Salandin, Emanuele <emanuele.salandin@phox-eng.it>
2018-06-19 08:47:17 +0200

    eplex (not working)

--- eclipse/hsp.pl
+++ eclipse/hsp.pl
@@ -1,7 +1,9 @@
 :- lib(listut).
 :- lib(between).
-:- lib(ic).
-:- lib(branch_and_bound).
+
+:- lib(eplex).
+:- eplex_instance(ep).
+
 
 ricetta([[50,200], [50,200], [50,200], [50,200], [50,200], [50,200], [50,200], [50,200], [50,200], [50,200]]).
 
@@ -28,48 +30,37 @@ event_t(List, Step, T) :-
 % Il formato e': [carico, pos1, pos2, ..., posN, scarico]
 
 lin0(Removal, Entry) :-
-	Removal=[R1|_], R1 $= 0,
-	Entry=[E1|_], E1 $= 0,
-	last(A, Removal), last(B, Entry), A $= B.
+	Removal=[R1|_],
+	Entry=[E1|_],
+	ep: (R1 $= 0.0),
+	ep: (E1 $= 0.0),
+	last(A, Removal), last(B, Entry),
+	ep: (A $= B).
 
 % vincolo: tra la estrazione da una posizione e l'inserimento nella successiva posizione deve esserci il tempo di sollevamento, traslazione, abbassamento.
 lin1(Removal, Entry) :- findall(S, step(S), StepsList), step_unload(Last), append(StepsList, [Last], StepsListExt), maplist(lin1(Removal, Entry), StepsListExt).
 
 lin1(Removal, Entry,S) :-
 	hoist_lift_t(TLift), hoist_lift_t(TLower),
-	succ(PredS, S), event_t(Removal, PredS, TA), hoist_move_t(PredS, S, TMove), event_t(Entry, S, TB), TB $= TA+ TLift+ TMove + TLower.
+	succ(PredS, S), event_t(Removal, PredS, TA), hoist_move_t(PredS, S, TMove), event_t(Entry, S, TB),
+	ep: (TB $= TA+ TLift+ TMove + TLower).
 	
 % vincolo: tempo di permanenza in una posizione
 lin2(Removal, Entry) :- findall(S, step(S), StepsList), maplist(lin2(Removal, Entry), StepsList).
 
 lin2(Removal, Entry, S) :-
 	event_t(Entry, S, TIn), event_t(Removal, S, TOut), step_t(S, Tmin, Tmax),
-	TIn+Tmin $=< TOut,
-	TIn+Tmax $>= TOut.
+	ep: (TIn+Tmin $=< TOut),
+	ep: (TIn+Tmax $>= TOut).
 
 % vincolo: il tempo di permanenza di una barra nel sistema non puo' superare NumJobs cicli (perche' ad ogni ciclo viene rimossa una barra)
 lin3(Removal, NumJobs, Period) :-
-	step_unload(ULStep), event_t(Removal, ULStep, LastT), hoist_move_t(ULStep,0,TRet), LastT + TRet $=< NumJobs * Period,
-	Period $=< LastT + TRet.
+	step_unload(ULStep), event_t(Removal, ULStep, LastT), hoist_move_t(ULStep,0,TRet),
+	ep: (LastT + TRet $=< NumJobs * Period),
+	ep: (Period $=< LastT + TRet).
 
 % il carro deve avere il tempo di andare da una posizione all'altra.
 % in questi calcoli il carro e' vuoto (non trasporta barre), quindi ignoriamo i tempi di sollevamento/abbassamento.
-
-% caso 1: la barra viene estratta dallo step2 dopo che e' stata estratta dallo step1
-disj1(Removal, Entry, Period, Step1, Step2, K) :- 
-	step_succ(Step1, SuccS1),
-	event_t(Entry, SuccS1, TEntrySuccS1),
-	hoist_move_t(SuccS1, Step2, TMoveSS1S2),
-	event_t(Removal, Step2, TRemovalS2),
-	TEntrySuccS1 + TMoveSS1S2 + K*Period $< TRemovalS2.
-	
-% caso 2: la barra viene estratta dallo step1 dopo che e' stata estratta dallo step2
-disj1(Removal, Entry, Period, Step1, Step2, K) :-
-	step_succ(Step2, SuccS2),
-	event_t(Entry, SuccS2, TEntrySuccS2),
-	hoist_move_t(SuccS2, Step1, TMoveSS2S1),
-	event_t(Removal, Step1, TRemovalS1),
-	TEntrySuccS2 + TMoveSS2S1 $< TRemovalS1 + K*Period.
 
 lin_disj1(Removal, Entry, Period, Step1, Step2, K) :-
 	step_succ(Step1, SuccS1),
@@ -80,10 +71,11 @@ lin_disj1(Removal, Entry, Period, Step1, Step2, K) :-
 	event_t(Entry, SuccS2, TEntrySuccS2),
 	hoist_move_t(SuccS2, Step1, TMoveSS2S1),
 	event_t(Removal, Step1, TRemovalS1),
-	[B1,B2] #:: 0..1,
-	$<(TEntrySuccS1 + TMoveSS1S2 + K*Period, TRemovalS2, B1),
-	$<(TEntrySuccS2 + TMoveSS2S1, TRemovalS1 + K*Period, B2),
-	B1+B2 #>= 1.
+	ep: ([B1,B2] $:: 0..1),
+	ep: integers([B1,B2]),
+	ep: (B1 => (TEntrySuccS1 + TMoveSS1S2 + K*Period $=< TRemovalS2)),
+	ep: (B2 => (TEntrySuccS2 + TMoveSS2S1 $=< TRemovalS1 + K*Period)),
+	ep: (B1+B2 $>= 1).
 	
 	
 	
@@ -122,3 +114,14 @@ check_period(Entry, Removal, NumJobs, Period) :-
 	length(Entry, ListLen), Entry :: 0..300000,
 	Period :: 0..30000000,
 	constraint(Removal, Entry, NumJobs, Period).
+	
+eplex_period(Entry, Removal, NumJobs) :-
+	steps_no(StepsNo), ListLen is StepsNo+2,
+	length(Removal, ListLen),
+	length(Entry, ListLen),
+	ep: (Removal $:: 0.0..300000.0),
+	ep: (Entry $:: 0.0..300000.0),
+	ep: (Period $:: 0.0..30000000.0),
+	constraint(Removal, Entry, NumJobs, Period),
+	ep: eplex_solver_setup(min(Period)),
+	ep: eplex_solve(Period).
