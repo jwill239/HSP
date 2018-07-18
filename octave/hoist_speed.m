4ad8c71724a1c3c095596d2631cdd71a1ee95392
Salandin, Emanuele <emanuele.salandin@phox-eng.it>
2018-07-18 11:12:31 +0200

    legge velocita'

--- octave/hoist_speed.m
+++ octave/hoist_speed.m
@@ -1,6 +1,18 @@
 function t= hoist_speed(x_mm)
-  hoist_ab_time=2; % acceleration_time e breaking_time
-  hoist_inching_time=1;
-  hoist_travel_speed= 583.333; % millimetri al secondo
-  t= x_mm/hoist_travel_speed;
+  hoist_ab_time=2; % acceleration_time e breaking_time (es. 2)
+  hoist_inching_time=1; % es. 1
+  hoist_travel_speed= 583.333/1000; % metri al secondo (es. 583.333/1000)
+
+  x= abs(x_mm)/1000;
+  if (x<eps)
+    t=0;
+    return;
+  endif
+  s_2ta= hoist_travel_speed^2/hoist_ab_time;
+  if (x < s_2ta)
+    t_mov= sqrt(x/hoist_travel_speed);
+  else
+    t_mov= 2*hoist_travel_speed/hoist_ab_time + (x - s_2ta)/hoist_travel_speed;
+  endif
+  t= t_mov + hoist_inching_time;
 endfunction
